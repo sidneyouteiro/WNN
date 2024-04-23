d5883280437584db1f74efb9a178c2b1755d1f89
alobogiron <andregiron.20221@poli.ufrj.br>
2024-04-23 11:34:51 -0300

    Add primary files and start comments to explain all functionalities.

---
+++ src/wisard.cc
@@ -0,0 +1,190 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Wisard {
+public:
+    //Nosso construtor
+    Wisard()
+    {
+        entrySize = 0;
+        tupleSize = 0;
+        numDiscriminator = 0;
+        discriminators = vector<Discriminator*>();
+    }
+
+    Wisard(int entrySize, int tupleSize, int numDiscriminator) :
+    //inicializa as variáveis com os valores definidos no construtor
+    //Member variable?
+    entrySize(entrySize), tupleSize(tupleSize), numDiscriminator(numDiscriminator)
+    {
+        int i;
+        discriminators = vector<Discriminator*>(numDiscriminator);
+        //Classe Discriminator
+
+        for (i = 0; i < numDiscriminator; i++) {
+            discriminators[i] = new Discriminator(entrySize, tupleSize);
+            //Cria os discriminadores com tamanho de entrada e tupla
+        }
+    }
+
+    //Destrutor
+    //Libera memória dinamicamente
+    ~Wisard()
+    {
+        unsigned int i;
+        for (i = 0; i < discriminators.size(); i++) {
+            delete discriminators[i];
+        }
+    }
+
+    //Incrementa o discriminador
+    void addDiscriminator()
+    {
+        //emplace back cria um novo último item
+        discriminators.emplace_back(new Discriminator(entrySize, tupleSize));
+        //incrementa o número de discriminadores
+        numDiscriminator++;
+    }
+
+    //data é um vetor de vetor do tipo booleano
+    //label é um vetor de inteiros
+    //O que é o label?
+    void train(const vector<vector<bool>>& data, const vector<int>& label)
+    {
+        unsigned int i;
+        
+        //percorre todo o vetor label
+        for (i = 0; i < label.size(); i++) {
+            //chama o método train do objeto discriminator apontado por discriminators[label[i]] e data[i] é o parâmetro
+            discriminators[label[i]]->train(data[i]);
+        }
+    }
+
+    //rankeamento?
+    int rank(const vector<bool>& data)
+    {
+        int i;
+        int label = 0;   
+        int max_resp = 0;
+        int resp;
+
+        //percorre todos os discriminadores
+        for (i = 0; i < numDiscriminator; i++) {
+            //chama ela mesma
+            resp = discriminators[i]->rank(data);
+
+            //o que é resp?
+            //checa resp com o resp máximo
+            // armazena o índice do resp máximo e retorna como label
+            if (resp > max_resp) {
+                max_resp = resp;
+                label = i;
+            }
+        }
+
+        return label;
+    }
+
+    //funçao do pybind
+    //py::array_t<int> rank(const vector<vector<bool>>& data)
+    //{
+        //Initialize results with numpy array
+        /*py::array_t<int> a({ data.size(), data.size() });
+        auto result = a.mutable_unchecked();*/
+        //py::array_t<int> a({data.size()});
+        //auto label = a.mutable_unchecked();
+
+        //unsigned int i;
+        //int j, max_resp, resp;
+        
+        //for (i = 0; i < data.size(); i++) {
+            //max_resp = 0;
+            //for ( j = 0; j < numDiscriminator; j++) {
+                //resp = discriminators[j]->rank(data[i]);
+
+                //if (resp > max_resp) {
+                    //max_resp = resp;
+                    //label(i) = j;
+                //}
+            //}
+        //}
+
+        //return a;
+    //}
+
+    //ex-func pybind
+    //preciso de voce?
+    //duas funcoes rank?
+    std::vector<int> rank(const vector<vector<bool>>& data)
+    {
+        std::vector<int> a({data.size()});
+
+        unsigned int i;
+        int j, max_resp, resp;
+
+        for(i = 0; i < data.size(); i++){
+            max_resp = 0;
+            for(i = 0; i < numDiscriminator; j++){
+                resp = discriminators[j]->rank(data[i]);
+
+                if(resp > max_resp){
+                    max_resp = resp;
+                    a[i] = j;
+                }
+            }
+        }
+
+    }
+
+
+
+    void info()
+    {
+        cout << "Number of Discriminators = " << numDiscriminator << endl;
+        int i;
+
+        for (i = 0; i < numDiscriminator; i++) {
+            cout << "Discriminator " << i << ": ";
+            discriminators[i]->info();
+        }
+    }
+
+    py::array_t<unsigned long int> stats()
+    {
+        py::array_t<unsigned long int> a({4});
+        auto stats = a.mutable_unchecked();
+
+        int numRams = discriminators[0]->getNumRams();
+        long int ramSize = discriminators[0]->getRamBits();
+        long int totalRamBits = numRams * ramSize; 
+        long int totalBits = numDiscriminator * totalRamBits;
+
+        stats(0) = numRams;
+        stats(1) = ramSize;
+        stats(2) = totalRamBits;
+        stats(3) = totalBits;
+
+        return a;
+    }
+
+    void reset()
+    {
+        int i;
+
+        for (i = 0; i < numDiscriminator; i++) {
+            discriminators[i]->reset();
+        }
+    }
+
+private:
+    int entrySize;
+    int tupleSize;
+    int numDiscriminator;
+    vector<Discriminator*> discriminators;
+};
+
+/**int main(){
+    return 0;
+}*/
