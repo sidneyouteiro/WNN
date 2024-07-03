2e90c17a25166f918751ab081c3a7a21aacf32d0
alobogiron <andregiron.20221@poli.ufrj.br>
2024-07-03 17:11:41 -0300

    Last updates

--- src/test_regress.cc
+++ src/test_regress.cc
@@ -1,2 +1,9 @@
 #include "wisardpkg.h"
 
+int main(){
+    std::vector<bool> a = {1,1,1,0,0,0,0,0,0};
+    std::vector<bool> a = {1,1,1,1,0,0,0,0,0};
+    std::vector<bool> a = {0,0,0,0,1,1,1,1,1};
+    std::vector<bool> a = {0,0,0,0,0,1,1,1,1};
+}
+
